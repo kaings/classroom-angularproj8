b295c1c2f2664396180fa4619f2a75acc2de0de9
willys_tjiawi <willys@huubap.com>
2020-02-21 00:24:50 +0800

    added tab & tab-panel

---
+++ src/tab-panel/tab-panel.component.ts
@@ -0,0 +1,24 @@
+import {AfterContentInit, Component, ContentChildren, QueryList} from '@angular/core';
+import {TabComponent} from '../tab-content/tab.component';
+
+@Component({
+  selector: 'tab-panel',
+  templateUrl: './tab-panel.component.html'
+})
+export class TabPanelComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabComponents: QueryList<TabComponent>;
+  tabs: TabComponent[];
+
+  ngAfterContentInit() {
+    this.tabs = this.tabComponents.toArray();
+    this.tabs[0].selected = true;
+
+    console.log(this.tabs);
+  }
+
+  selectTab(tabComponent: TabComponent) {
+    console.log('index..... ', tabComponent);
+    this.tabs.map((tab: TabComponent) => tab.selected = false);
+    tabComponent.selected = true;
+  }
+}
