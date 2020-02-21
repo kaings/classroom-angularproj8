67fae9d620914bfe3fa2727c50c79a6dfbda3a86
willys_tjiawi <willys@huubap.com>
2020-02-21 23:04:37 +0800

    passing ngForTemplate dynamically

--- src/skill-list/skill-list.component.ts
+++ src/skill-list/skill-list.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, ContentChild, Input, TemplateRef} from '@angular/core';
+import {NgForOfContext} from '@angular/common';
 
 @Component({
   selector: 'skill-list',
   template: `
-    <ng-template ngFor let-skill [ngForOf]="skills">
-      <div class="media d-flex m-2">
+    <ng-template ngFor let-skill [ngForOf]="skills" 
+                 [ngForTemplate]="customSkillListTemplate ? customSkillListTemplate : defaultSkillListTemplate">
+      <div class="media d-flex m-2" #defaultSkillListTemplate>
         <img class="mx-5 icon" [src]="skill.icon" alt="skill icon" style="">
         <div class="media-body align-self-center">{{skill.name}}</div>
       </div>
@@ -13,4 +15,5 @@ import {Component, Input} from '@angular/core';
 })
 export class SkillListComponent {
   @Input() skills: {[key: string]: string}[];
+  @ContentChild(TemplateRef) customSkillListTemplate: TemplateRef<NgForOfContext<any>>;
 }
