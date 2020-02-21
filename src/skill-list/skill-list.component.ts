47a4751463509e01230cc13ea689afda33b27aef
willys_tjiawi <willys@huubap.com>
2020-02-21 18:17:21 +0800

    modified ngFor into its original form (ng-template)

--- src/skill-list/skill-list.component.ts
+++ src/skill-list/skill-list.component.ts
@@ -3,12 +3,12 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'skill-list',
   template: `
-    <div *ngFor="let skill of skills">
+    <ng-template ngFor let-skill [ngForOf]="skills">
       <div class="media d-flex m-2">
         <img class="mx-5 icon" [src]="skill.icon" alt="skill icon" style="">
         <div class="media-body align-self-center">{{skill.name}}</div>
       </div>
-    </div>
+    </ng-template>
   `
 })
 export class SkillListComponent {
